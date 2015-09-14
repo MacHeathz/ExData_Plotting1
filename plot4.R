2fcb1363c5ae3511ff1918eed4f9d6c78cb2b2ee
MacHeathz <MacHeathz@users.noreply.github.com>
2015-09-14 12:41:11 +0200

    Plot4 last refinements

--- plot4.R
+++ plot4.R
@@ -9,7 +9,7 @@ par(mfrow = c(2,2))
 
 # Create the first plot, in topleft position
 with(data, plot(datetime, Global_active_power, type = "l",
-                ylab = "Global Active Power (kilowatts)", xlab = ""))
+                ylab = "Global Active Power", xlab = ""))
 
 # Create the second plot, in topright position
 with(data, plot(datetime, Voltage, type = "l"))
@@ -22,7 +22,7 @@ lines(dt, data$Sub_metering_2, type = "l", col = "red")
 lines(dt, data$Sub_metering_3, type = "l", col = "blue")
 legend(x = "topright",
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       lty = c(1,1,1), col = c("black", "red", "blue"))
+       lty = c(1,1,1), col = c("black", "red", "blue"), bty = "n")
 
 # Create the fourth plot, in bottomright position
 with(data, plot(datetime, Global_reactive_power, type = "l"))
